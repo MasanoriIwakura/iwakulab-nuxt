f7df2dbf6cfb0cb0186e4afd4e1b247a706a585b
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-04-12 00:31:16 +0900

    Update Navbar
    responsive settings

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,39 @@
 <template>
-  <v-toolbar class="grey darken-4" dark>
-    <v-toolbar-title>
-      <img src="/img/iwakulab-logo.svg" width="64" height="64">
-    </v-toolbar-title>
+  <nav>
+    <v-toolbar class="grey darken-4 hidden-sm-and-down" dark>
+      <v-toolbar-title>
+        <router-link to="/">
+          <img src="/img/iwakulab-logo.svg" width="64" height="64">
+        </router-link>
+      </v-toolbar-title>
 
-    <v-divider class="mx-3" inset vertical></v-divider>
+      <v-divider class="mx-3" inset vertical></v-divider>
 
-    <v-toolbar-items>
-      <v-btn flat v-for="item in menu" :key="item.title" :to="item.link">{{ item.title }}</v-btn>
-      <v-divider vertical></v-divider>
-    </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn flat v-for="item in menu" :key="item.title" :to="item.link">{{ item.title }}</v-btn>
+        <v-divider vertical></v-divider>
+      </v-toolbar-items>
 
-    <v-spacer></v-spacer>
-    <v-btn flat icon href="https://github.com/MasanoriIwakura/iwakulab-nuxt" target="_blank">
-      <v-icon medium>fa-github</v-icon>
-    </v-btn>
-  </v-toolbar>
+      <v-spacer></v-spacer>
+      <v-btn flat icon href="https://github.com/MasanoriIwakura/iwakulab-nuxt" target="_blank">
+        <v-icon medium>fa-github</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <div class="hidden-md-and-up">
+      <v-expansion-panel class="grey darken-4" dark>
+        <v-expansion-panel-content v-model="show">
+          <div slot="header">
+            <router-link to="/">
+              <img src="/img/iwakulab-logo.svg" width="32" height="32">
+            </router-link>
+          </div>
+          <v-card v-for="item in menu" :key="item.title">
+            <v-btn flat class="nav-item" :to="item.link" style="width:100%;" @click="closeMenu">{{ item.title }}</v-btn>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -23,12 +41,17 @@ export default {
   data() {
     return {
       menu: [
-        { title: 'Home', link: '/' },
         { title: 'Profile', link: '/profile' },
         { title: 'Tech Blog', link: '/blog' },
         { title: 'Hobby Blog', link: '/hobby' },
         { title: 'Contact', link: '/contact' }
-      ]
+      ],
+      show: false
+    }
+  },
+  methods: {
+    closeMenu () {
+      this.show = false
     }
   }
 }
